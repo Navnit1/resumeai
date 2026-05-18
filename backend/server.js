3d963327e3ad4d9715c3b73d8ccc43645b854cf3
Your Name <you@example.com>
2026-05-18 17:15:49 +0530

    fix cors origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(helmet());
 app.use(xss());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL ,
   credentials: true,
 }));
 

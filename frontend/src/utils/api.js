76c21610029f98c50bc6b8a49d5fe4b1ab8a9ec6
Your Name <you@example.com>
2026-05-18 18:17:02 +0530

    fix :remove localhost & update api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_BASE =
-  process.env.REACT_APP_API_URL ||
-  " ";
+  process.env.REACT_APP_API_URL 
+  ;
 
 // Create axios instance
 const api = axios.create({
